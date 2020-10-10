26a70318912a7e4e5a0eb0c13cba27eb6f0dfae9
sksum <sakshammrigdgr8@gmail.com>
2020-10-10 18:42:06 +0530

    bug

--- client/src/components/LOGO.js
+++ client/src/components/LOGO.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../img/logo.png';
 
-export default function LOGO() {
+export default function LOGO({history}) {
   return (
         <div className="enter-room-container">
             <img src={logo} alt=""/>

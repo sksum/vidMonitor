e0f0f2354f8e7ca6ec210c7b999e1502acc6e72a
sksum <sakshammrigdgr8@gmail.com>
2020-10-10 16:21:15 +0530

    tests

--- server/server.js
+++ server/server.js
@@ -2,14 +2,33 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socket = require("socket.io");
-const io =  socket(server);
+const socketIo = require("socket.io");
+const io =  socketIo(server);
 
 const rooms = {};
+const tests = {};
+const invils = {};
 
 io.sockets.on("connection", socket => {
     console.log(`connected: ${socket.id} `)
 
+    socket.on("Invil=>Server:letsInitialize",(id) => {
+        console.log(`making new test :${id} + socket.id of invil = ${socket.id}`)
+        tests[id] = [socket.id];
+        invils[id] = socket.id;
+    })
+
+    socket.on("Stud=>Server:letsInitialize",(id) => {
+        console.log(` Student socket.id : ${socket.id} entered exam room- ${id}`)
+        if (tests[id]) {
+            tests[id].push(socket.id);
+            const invil = invils[id];
+            socket.emit("Server=>Stud:invilFound", invil);
+            socket.to(invil).emit("Server=>Invil:newStudent", socket.id);
+            
+        }   
+    })
 
 
 });
+server.listen(8000 ,() => {console.log("server.js running")});

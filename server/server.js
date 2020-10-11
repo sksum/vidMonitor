76bbfbefddc97f00c43f2951f37743b5e2b00ee4
sksum <sakshammrigdgr8@gmail.com>
2020-10-11 09:07:57 +0530

    server

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const server = http.createServer(app);
 const socketIo = require("socket.io");
 const io =  socketIo(server);
 
-const rooms = {};
 const tests = {};
 const invils = {};
 
+
 io.sockets.on("connection", socket => {
     console.log(`connected: ${socket.id} `)
-
+    
     socket.on("Invil=>Server:letsInitialize",(id) => {
         console.log(`making new test :${id} + socket.id of invil = ${socket.id}`)
         tests[id] = [socket.id];
@@ -19,7 +19,7 @@ io.sockets.on("connection", socket => {
     })
 
     socket.on("Stud=>Server:letsInitialize",(id) => {
-        console.log(` Student socket.id : ${socket.id} entered exam room- ${id}`)
+        console.log(` Student socket.id : ${socket.id} entered exam room ${id}`)
         if (tests[id]) {
             tests[id].push(socket.id);
             const invil = invils[id];
@@ -29,6 +29,20 @@ io.sockets.on("connection", socket => {
         }   
     })
 
+    socket.on("offer", payload => {
+        console.log(`offered :${payload.target} :: ${socket.id}`)
+        io.to(payload.target).emit("offer_received",payload)
+    })
+    
+    socket.on("answer", payload => {
+        console.log(`answered :${payload.target} :: ${socket.id}`)
+        io.to(payload.target).emit("answer_received",payload)
+    })
 
+    socket.on("ice-candidate", incoming => {
+        console.log(`iced :${incoming.target} :: ${socket.id}`)
+        io.to(incoming.target).emit("ice-candidate_received",incoming.candidate)
+    })
 });
-server.listen(8000 ,() => {console.log("server.js running")});
+
+server.listen(8000 ,() => {console.log("server2 running")});
